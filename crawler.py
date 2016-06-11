011cb94ffa9645156140a72fb5f39b304edc5f53
iamacewhite <ace10101@126.com>
2016-06-11 15:35:19 +0800

    basic function finished

--- crawler.py
+++ crawler.py
@@ -8,13 +8,13 @@ Created on Wed Jun  8 17:36:58 2016
 from twitter import *
 from langdetect import detect
 from datetime import datetime
-import json, gzip
+import json, gzip, os, traceback
 global DIR, cache1, cache2, MINIBATCH_SIZE, MAX_CACHE
 DIR = 'tweets/'
 cache1 = []
 cache2 = []
-MINIBATCH_SIZE = 160
-MAX_CACHE = 3000
+MINIBATCH_SIZE = 100
+MAX_CACHE = 200
 
 
 def sendMiniBatch():
@@ -27,17 +27,24 @@ def sendMiniBatch():
                 cache2 = []
     except Exception as e:
         print(e)
+        traceback.print_exc()
+        raise SystemExit
 
 def writeData():
-    time = datetime.strftime(datetime.now(), "%Y-%m-%d %H-%M-%S")
-    filename = DIR + time + ".json"
-    with open(filename, 'wb') as file:
-        for obj in cache2:
-            file.write(obj)
-    with open(filename) as f_in, gzip.open(filename + '.gz', 'wb') as f_out:
-        f_out.writelines(f_in)
-    os.remove(filename)
-    print(" - written " + len(cache2) + " tweets to files.")
+    try:
+        time = datetime.strftime(datetime.now(), "%Y-%m-%d %H-%M-%S")
+        filename = time + ".json"
+        with open(os.path.join(DIR, filename), 'wb') as file:
+            for obj in cache2:
+                file.write(obj)
+        with open(os.path.join(DIR, filename), 'rb') as f_in, gzip.open(os.path.join(DIR, filename + '.gz'), 'wb') as f_out:
+            f_out.writelines(f_in)
+        os.remove(os.path.join(DIR, filename))
+        print(" - written " + str(len(cache2)) + " tweets to files.")
+    except Exception as e:
+        print(e)
+        traceback.print_exc()
+        raise SystemExit
 
 twitter_stream = TwitterStream(auth=OAuth(
                        "226621933-UjykmlKE2K2XAH9EJQPALqzite4iODoI0dj2WxOw", # OAUTH_TOKEN
@@ -62,9 +69,3 @@ while True:
                     cache1 = []
         except Exception as e:
             print(e)
-
-        
-
-
-        
-    
